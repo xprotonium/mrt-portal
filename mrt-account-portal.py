e8c684972bdb3ea724708e965c6293bb55a75a21
JoonyLee0118 <chunhuilee0118@gmail.com>
2025-01-23 22:29:29 +0800

    done
    email and id

--- mrt-account-portal.py
+++ mrt-account-portal.py
@@ -4,7 +4,7 @@
 # Lecture / Lab Section: TC4L / TL9L
 # Trimester: 2430
 # Names: ALI SAJULAKE ABDUL GAFUR | NAHIN BIN MONIR | LEE CHUN HUI
-# IDs: 243FC245YU | MEMBER_ID_2 | MEMBER_ID_3
-# Emails: abdul.gafur.ali@student.mmu.edu.my | MEMBER_EMAIL_2 | MEMBER_EMAIL_3
+# IDs: 243FC245YU | MEMBER_ID_2 | 243FC245WJ
+# Emails: abdul.gafur.ali@student.mmu.edu.my | MEMBER_EMAIL_2 | LEE.CHUN.HUI@student.mmu.edu.my
 # *************************************************************************
 
