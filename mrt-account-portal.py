13754772273965dfeabd2ac82a38e9f756bf5755
Ali <westbrookcricket1@gmail.com>
2025-01-23 19:12:27 +0800

    setup

---
+++ mrt-account-portal.py
@@ -0,0 +1,10 @@
+# *************************************************************************
+# Program: mrt-account-portal.py
+# Course: CSP1114 PROBLEM SOLVING AND PROGRAM DESIGN
+# Lecture / Lab Section: TC4L / TL9L
+# Trimester: 2430
+# Names: ALI SAJULAKE ABDUL GAFUR | NAHIN BIN MONIR | LEE CHUN HUI
+# IDs: 243FC245YU | MEMBER_ID_2 | MEMBER_ID_3
+# Emails: abdul.gafur.ali@student.mmu.edu.my | MEMBER_EMAIL_2 | MEMBER_EMAIL_3
+# *************************************************************************
+
