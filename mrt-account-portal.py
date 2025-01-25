9959e9bcd381cd4b3be12723dfb5ba4162904417
Nahin Bin Monir <nahinbinmunir@gmail.com>
2025-01-25 19:32:49 +0800

    Update mrt-account-portal.py

--- mrt-account-portal.py
+++ mrt-account-portal.py
@@ -4,7 +4,7 @@
 # Lecture / Lab Section: TC4L / TL9L
 # Trimester: 2430
 # Names: ALI SAJULAKE ABDUL GAFUR | NAHIN BIN MONIR | LEE CHUN HUI
-# IDs: 243FC245YU | MEMBER_ID_2 | 243FC245WJ
-# Emails: abdul.gafur.ali@student.mmu.edu.my | MEMBER_EMAIL_2 | LEE.CHUN.HUI@student.mmu.edu.my
+# IDs: 243FC245YU | 243FC245H1 | 243FC245WJ
+# Emails: abdul.gafur.ali@student.mmu.edu.my | monir.nahin@gmail.com| LEE.CHUN.HUI@student.mmu.edu.my
 # *************************************************************************
 
