31b06f2627f22c5b77199ec285aff133f2cb9694
Ali <westbrookcricket1@gmail.com>
2025-02-07 04:15:23 +0800

    Function changes and loop fixes
    changes made:
    
    - replaced `fare_calculator` with a new function called `buy_ticket`
        This new function now displays the station names and allows the user to choose a starting station and a destination station.
        After the user selects them, the program asks for the amount of tickets user wants to buy.
        The program will then check the total price for the tickets and checks if the user balance > price.
        If it is, it will subtract the price from the balance.
        If not the transaction will not process returning the user to the `main` function
    - added the ability to cancel process mid payment, this ensures the user is not stuck in a loop
    - changed the registration logic in the `main` function, now right after the user enters the username, the programs checks if it is taken or not.
    - added a function called `clear` that basically clears the terminal after a few processes or commands.

--- mrt-account-portal.py
+++ mrt-account-portal.py
@@ -11,6 +11,8 @@
 import sqlite3
 import hashlib
 from tabulate import tabulate
+import os
+from time import sleep
 
 # connection
 connection = sqlite3.connect('account.db')
@@ -27,6 +29,7 @@ cursor.execute('CREATE TABLE IF NOT EXISTS account (username TEXT PRIMARY KEY, p
 # select all data from account table
 # and print all data in a table format
 def admin():
+    clear()
     cursor.execute('SELECT * FROM account')
     data = cursor.fetchall()
     # Get column names
@@ -36,22 +39,17 @@ def admin():
     print(tabulate(data, headers=columns, tablefmt='grid'))
 
 
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
 # register, login, logout function - ALI
 def register(username, password, name):
-    # check if the username exists in the database or not
-    cursor.execute('SELECT username FROM account WHERE username = ?', (username,))
-    row = cursor.fetchone()
-
-    if row:
-        print('Username is taken. Please try again')
-    else:
-        # hash the password for security
-        password = hashlib.sha256(password.encode()).hexdigest()
-        cursor.execute('INSERT INTO account (username, password, name, balance) VALUES (?, ?, ?, 0)',
-                       (username, password, name))
-        connection.commit()
-        return True
-    return False
+    # hash the password for security
+    password = hashlib.sha256(password.encode()).hexdigest()
+    cursor.execute('INSERT INTO account (username, password, name, balance) VALUES (?, ?, ?, 0)',
+                   (username, password, name))
+    connection.commit()
 
 
 def login(username, password):
@@ -69,6 +67,7 @@ def login(username, password):
             saved_name_row = cursor.fetchone()
             saved_name = saved_name_row[0]
             print(f'Logged in successfully.\nWelcome {saved_name}!')
+            sleep(1)
             return True
         else:
             print('Incorrect password.')
@@ -93,9 +92,15 @@ def view_name(username):
 
 # edit name - NAHIN
 def edit_name(username, new_name):
-    cursor.execute("UPDATE account SET name = ? WHERE username = ?", (new_name, username))
-    connection.commit()
-    print("Name changed successfully")
+    cursor.execute('SELECT username FROM account WHERE username = ?', (username,))
+    row = cursor.fetchone()
+
+    if row:
+        print('Username is taken. Please try again')
+    else:
+        cursor.execute("UPDATE account SET name = ? WHERE username = ?", (new_name, username))
+        connection.commit()
+        print("Name changed successfully")
 
 
 # edit username - NAHIN
@@ -129,9 +134,17 @@ def reload_balance(username, amount):
     else:
         print("It seems like you didn't add any funds. Consider reloading your balance soon!")
 
+    sleep(2)
 
-# fare calculator - ALI
-def fare_calculator():
+
+# buy ticket - ALI
+def buy_ticket(username):
+    # clear the screen before displaying contents from the function
+    clear()
+
+    # initialize variables
+    invalid_entries = 0
+
     stations = ['PUTRAJAYA SENTRAL',
                 'CYBERJAYA CITY CENTRE',
                 'CYBERJAYA UTARA',
@@ -152,59 +165,129 @@ def fare_calculator():
 
     # prompt the user for the starting station and destination station
     while True:
-        starting = input('Enter starting station: ').upper().strip()
-        destination = input('Enter destination: ').upper().strip()
+        if invalid_entries == 2:
+            invalid_entries = 0
+            sleep(1)
+            clear()
+
+        starting = input('Enter starting station: ').strip().upper()
+        destination = input('Enter destination: ').strip().upper()
 
         # check if the entered stations exist on the stations list
         if starting not in stations or destination not in stations:
             print('Invalid station name entered. Please check if the station exists on the list.')
+            invalid_entries += 1
         else:
             break
 
-    # calculate the fare
+    # calculate the price
     # to do this, index of each station is taken and the absolute value is calculated to imitate distance
     starting_index = stations.index(starting)
     destination_index = stations.index(destination)
     number_of_stations = abs(destination_index - starting_index)
     fare = 2 * number_of_stations
-    print(f'The fare from {starting} station -> {destination} station is RM{fare}')
+
+    while True:
+        # prompt the user for the amount of tickets
+        tickets = input('Enter number of tickets you want\nType cancel to cancel\n>> ')
+
+        # tickets acts like a command line, waiting for two different types of input, if the user enters a digit, continue ticket transaction
+        # if not, check of the string entered is 'cancel', if it is, cancel the transaction
+        if tickets.isdigit() and int(tickets) > 0:
+            tickets = int(tickets)
+            price = fare * tickets
+            cmd = input(
+                f'The fare from {starting} station -> {destination} station is RM{fare}\nThe total price for {tickets} ticket(s) is RM{price}\nDo you wish to proceed?\nYes [Y] No [N]\n>>').lower()
+            if cmd == 'y' or cmd == 'yes':
+                cursor.execute('SELECT balance FROM account WHERE username = ?', (username,))
+                row = cursor.fetchone()
+                if row:
+                    user_balance = row[0]
+
+                    if user_balance > price:
+                        cursor.execute('UPDATE account SET balance = balance - ? WHERE username = ?',
+                                       (price, username,))
+                        connection.commit()
+                        print('transaction successful\nreturning...')
+                        sleep(2)
+                        break
+                    else:
+                        print('transaction failed\nyour balance is insufficient.')
+                        sleep(2)
+                        return
+                else:
+                    print('An error occurred...')
+            elif cmd == 'n' or cmd == 'no':
+                print('cancelling transaction\nreturning...')
+                sleep(2)
+                return
+            else:
+                print('invalid command entered')
+                return
+            break
+        elif tickets.lower() == "cancel":
+            print('cancelling process...\nreturning')
+            sleep(2)
+            break
+        else:
+            print('Invalid amount entered.')
 
 
 # main function - ALI, NAHIN, LEE
 def main():
+    clear()
     print('WELCOME TO THE MRT ACCOUNT PORTAL')
     # check if the user is logged in or not, some functions are meant to be executed only if the user is logged in.
     # another reason for this check is that if the user is already logged in, do not allow the user to log in again.
     logged_in = False
     username = None
+
+    # keep track of how many commands entered
+    # after the user enters 5 commands clear the screen
+    cmd_counter = 0
+
     while True:
+        if cmd_counter >= 2:
+            cmd_counter = 0
+            sleep(1)
+            clear()
+
         cmd = input('type "help" for list of commands.\n>> ').strip().lower()
 
         # check for the different commands
         if cmd == 'register':
             if logged_in:
                 print('Cannot register, already logged in. Please logout if you want to make another account')
+
             else:
                 while True:
                     username = input('Enter username: ')
-                    while True:
-                        password = input('Enter password: ')
-                        confirm_password = input('Confirm password: ')
-                        if confirm_password == password:
-                            break
-                        else:
-                            print('Passwords do not match, please try again:')
+                    # check if the username exists in the database or not
+                    cursor.execute('SELECT username FROM account WHERE username = ?', (username,))
+                    result = cursor.fetchone()
 
-                    name = input('Enter name: ')
-                    if register(username, password, name):
-                        print('Account registered successfully.\nYou can log in to your account now.')
-                        break
+                    if result:
+                        print('Username is taken. Please try again')
                     else:
-                        print('Registration failed. Please try again.')
+                        while True:
+                            password = input('Enter password: ')
+                            confirm_password = input('Confirm password: ')
+                            if confirm_password == password:
+                                break
+
+                            else:
+                                print('Passwords do not match, please try again:')
+
+                        name = input('Enter name: ')
+                        if register(username, password, name):
+                            print('Account registered successfully.\nYou can log in to your account now.')
+                            break
+
         elif cmd == 'login':
             # check if the user is already logged in or not
             if logged_in:
                 print('Already logged in.')
+
             else:
                 # prompt for username and password
                 username = input('Enter username: ')
@@ -215,46 +298,57 @@ def main():
                     cursor.execute("SELECT username FROM account WHERE username = ?", (username,))
                     result = cursor.fetchone()
                     username = result[0]
+
                 else:
                     logged_in = False
+
         elif cmd == 'logout':
             logout()
         elif cmd == 'view name':
             if not logged_in:
                 print('You are not logged in.')
+
             else:
                 print(view_name(username))
+
         elif cmd == 'edit name':
             if not logged_in:
                 print('You are not logged in.')
+
             else:
                 username = username
                 new_name = input("Enter new name: ")
                 edit_name(username, new_name)
+
         elif cmd == 'edit username':
             if not logged_in:
                 print('You are not logged in.')
+
             else:
                 username = username
                 new_username = input("Enter a new username: ")
                 edit_username(username, new_username)
                 username = new_username
+
         elif cmd == 'check balance':
             if not logged_in:
                 print('You are not logged in.')
+
             else:
                 username = username
                 check_balance(username)
+
         elif cmd == 'reload balance':
             if not logged_in:
                 print("You are not logged in.")
+
             else:
                 username = username
                 while True:
                     amount = input("Enter amount\nType cancel to cancel process\n>> ").strip()
                     # check if the input entered is digit or not
                     # if it is, convert it to an integer
-                    if amount.isdigit():
+                    if amount.isdigit() and int(amount) > 0:
                         amount = int(amount)
                         reload_balance(username, amount)
                         break
@@ -262,18 +356,27 @@ def main():
                         break
                     else:
                         print("Invalid amount entered. Please try again.")
-        elif cmd == 'fare calculator' or cmd == 'fare' or cmd == 'fare calc':
+
+        elif cmd == 'buy tickets' or cmd == 'buy ticket' or cmd == 'ticket' or cmd == 'tickets' or cmd == 'buy':
             if not logged_in:
                 print('You are not logged in.')
             else:
-                fare_calculator()
+                buy_ticket(username)
+                cmd_counter += 1
         elif cmd == 'admin':
+            if cmd_counter == 1:
+                cmd_counter -= 1
             admin()
         elif cmd == 'help':
+            if cmd_counter == 1:
+                cmd_counter -= 1
+            clear()
             print(
                 'register\nlogin\nlogout\nview name\nedit name\nedit username\ncheck balance\nreload balance\nfare calculator')
         else:
             print('Invalid command')
+
+        cmd_counter += 1
 
 
 # call main function
